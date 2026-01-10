f24fafbcbe9fc6c8bebf518938180726194b0820
minh <minhnhangia@gmail.com>
2026-01-10 15:59:15 +0800

    cleanup + use sime time

--- launch/custom_ekf.launch.py
+++ launch/custom_ekf.launch.py
@@ -1,18 +1,3 @@
-# Copyright 2018 Open Source Robotics Foundation, Inc.
-# Copyright 2019 Samsung Research America
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 import launch_ros.actions
@@ -35,25 +20,39 @@ def generate_launch_description():
         description='Name of the robot (namespace)'
     )
 
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true'
+    )
+
+    ekf_node = launch_ros.actions.Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node',
+        output='screen',
+
+        # ---------------------------------------------------------
+        # 1. NAMESPACE: Sets the scope to /<robot_name>
+        # ---------------------------------------------------------
+        namespace=robot_name,
+
+        parameters=[
+            # Path to the YAML file
+            os.path.join(get_package_share_directory("robot_localization"), 'params', 'ekf.yaml'),
+            {'use_sim_time': use_sim_time}
+        ],
+
+        # ---------------------------------------------------------
+        # 2. REMAPPING: ('/tf', 'tf') becomes '/<robot_name>/tf'
+        # ---------------------------------------------------------
+        remappings=[('/tf', 'tf')]
+    )
+
     return LaunchDescription([
         robot_name_arg,
-        
-        launch_ros.actions.Node(
-            package='robot_localization',
-            executable='ekf_node',
-            name='ekf_filter_node',
-            output='screen',
-            # ---------------------------------------------------------
-            # 1. NAMESPACE: Sets the scope to /<robot_name>
-            # ---------------------------------------------------------
-            namespace=robot_name,
-            
-            parameters=[os.path.join(get_package_share_directory("robot_localization"), 'params', 'ekf.yaml')],
-            
-            # ---------------------------------------------------------
-            # 2. REMAPPING: ('/tf', 'tf') becomes '/<robot_name>/tf'
-            # ---------------------------------------------------------
-            remappings=[('/tf', 'tf')] 
-        ),
+        use_sim_time_arg,
+        ekf_node
     ])
 
